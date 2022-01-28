820d12ee1b9fbb5f54f4ae946442377f3b09c4a1
charula63 <68392525+charula63@users.noreply.github.com>
2022-01-28 15:56:23 +0530

    Add files via upload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,1 +1,17 @@
+import Todo from './components/Todo'
 
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Laundry' />
+      <Todo text='Watering the plants' />
+      <Todo text='Painting' />
+      
+
+    </div>
+  );
+}
+
+export default App; 
